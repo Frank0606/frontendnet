6c981d4cc9c2e4ba9a0510d14f8f5780a32d35e7
Frank0606 <frankdanielsanchez55@gmail.com>
2024-04-19 18:58:52 -0600

    Practica PHP

---
+++ mvcPHP/app/routes.php
@@ -0,0 +1,7 @@
+<?php
+use Slim\App;
+
+return function(App $app){
+    $app->get('/', 'App\Controllers\HomeController:index');
+    //$app->get('/home', 'App\Controllers\HomeController:index');
+};
